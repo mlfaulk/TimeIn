bf8eb5a9cdf4a1fafbf5575a7df37845993c9f0c
Maggie Goulder <mgoulder@gmail.com>
2015-05-19 14:29:50 -0700

    added teacher phone number and character scenarios good/bad

--- db/migrate/20150518233114_create_resources.rb
+++ db/migrate/20150518233114_create_resources.rb
@@ -2,7 +2,9 @@ class CreateResources < ActiveRecord::Migration
   def change
     create_table :resources do |t|
     	t.string	:char_scen_img_file
+    	t.integer	:good_char 		# 1 = good character scenario, 0 = bad character scenario
     	t.string	:caption
+
       t.timestamps
     end
   end
